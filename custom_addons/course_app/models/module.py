74c331efc2d1b95b424a3077405de90fa2bd612c
Dono97 <42067647+Dono97@users.noreply.github.com>
2019-10-17 13:31:14 +0200

    Added button and it works

--- custom_addons/course_app/models/module.py
+++ custom_addons/course_app/models/module.py
@@ -7,7 +7,7 @@ class Module(models.Model):
     _description = 'Module'
     name = fields.Char('Module name', required=True)
     year = fields.Char('Module year', required=True)
-    code = fields.Char('Module code', compute='_compute_module_code', required=True)
+    code = fields.Char('Module code', compute='compute_module_code', required=True)
 
     #Relationships
     program_module_id = fields.Many2one('program.course', string='Program')
@@ -15,10 +15,11 @@ class Module(models.Model):
     lecturer_module_id = fields.Many2one('lecturer.user', string='Lecturer')
     transcript_module_id = fields.Many2one('transcript.student',string='Transcript')
 
-@api.multi
-def compute_module_code(self):
-    self.ensure_one()
-    self.code = self.name[:3] + self.year + "0"
-    while self.code.upper() in module.program:
-        self.code = self.name[:3] + self.year + str(int(self.code[-1])+1)
-        return self.code
+    @api.multi
+    def compute_module_code(self):
+        self.ensure_one()
+        self.code = self.name[:3] + self.year + "0"
+        while self.code.upper() in self:
+            self.code = self.name[:3] + self.year + str(int(self.code[-1])+1)
+            return self.code
+            
