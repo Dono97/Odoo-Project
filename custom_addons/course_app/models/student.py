dcfb337bf507ebe596c2faa5dc48d9a5e24d0b77
Dono97 <42067647+Dono97@users.noreply.github.com>
2019-10-21 10:54:16 +0200

    Functions done thanks angus

--- custom_addons/course_app/models/student.py
+++ custom_addons/course_app/models/student.py
@@ -5,7 +5,7 @@ from odoo.exceptions import Warning
 class Student(models.Model) :
     _name = 'student.user'
     _description = 'Student'
-    # student_number = fields.Char('Student Number', compute='compute_stu_num', required=True)
+    student_number = fields.Char('Student Number', compute='compute_stu_num')
     first_name = fields.Char('First Name', required=True)
     last_name = fields.Char('Last Name', required=True)
 
@@ -16,5 +16,5 @@ class Student(models.Model) :
 
     @api.multi
     def compute_stu_num(self):
-        self.ensure_one()
-        self.student_number = '100'
+        for rec in self:
+            rec.student_number = rec.id
